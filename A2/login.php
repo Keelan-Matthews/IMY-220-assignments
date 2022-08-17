5ebf990fc8604c5b0826b0f96153f3f1e09a6849
Keelan <keelanmatthews123@gmail.com>
2022-08-17 20:17:38 +0200

    first A3 commit

--- A2/login.php
+++ A2/login.php
@@ -1,18 +1,59 @@
 <?php
-	// See all errors and warnings
-	error_reporting(E_ALL);
-	ini_set('error_reporting', E_ALL);
+// See all errors and warnings
+error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
 
-	$mysqli = mysqli_connect("localhost", "root", "", "dbuser");
+$mysqli = mysqli_connect("localhost", "root", "", "dbuser");
 
-	$email = isset($_POST["email"]) ? $_POST["email"] : null;
-	$pass = isset($_POST["pass"]) ? $_POST["pass"] : null;
-	// If email and/or pass POST values are set, set the variables to those values, otherwise make them false
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$email = isset($_POST["email"]) ? test_input($_POST["email"]) : null;
+	$res = $mysqli->query("SELECT `user_id` FROM tbusers WHERE email = '$email'");
+	if ($row = mysqli_fetch_array($res))
+		$id = $row["user_id"];
 
+	$pass = isset($_POST["pass"]) ? test_input($_POST["pass"]) : null;
+	$eventName = isset($_POST["eventName"]) ?  test_input($_POST["eventName"]) : null;
+	$eventDescription = isset($_POST["eventDescription"]) ? test_input($_POST["eventDescription"]) : null;
+	$eventDate = isset($_POST["eventDate"]) ? $_POST["eventDate"] : null;
+	$filename = isset($_FILES['picToUpload']['name'][0]) ? $_FILES['picToUpload']['name'][0] : null;
+
+	if ($eventName != null && $eventDescription != null && $eventDate != null && $filename != null) {
+
+		$target_dir = "gallery/";
+
+		$allowedFiles =  array('jpg', 'jpeg');
+		$ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+
+		if ($_FILES['picToUpload']['size'][0] < 1000000 && in_array($ext, $allowedFiles)) {
+			$target_file = $target_dir . $filename;
+			move_uploaded_file($_FILES['picToUpload']['tmp_name'][0], $target_file);
+		} else {
+			echo "File is too big or not an image";
+			exit();
+		}
+
+		$mysqli->query("INSERT INTO tbevents (`user_id`, `name`, `description`, `date`) VALUES ($id, '$eventName', '$eventDescription', '$eventDate')");
+
+		$res = $mysqli->query("SELECT event_id FROM tbevents WHERE `user_id` = $id AND `name` = '$eventName'");
+		if ($res && $row = mysqli_fetch_array($res))
+			$eventId = $row["event_id"];
+
+		$mysqli->query("INSERT INTO tbgallery (event_id, image_name) VALUES ($eventId, '$filename')");
+	}
+}
+
+function test_input($data)
+{
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>IMY 220 - Assignment 2</title>
 	<meta charset="utf-8" />
@@ -22,14 +63,14 @@
 	<meta name="author" content="Keelan Matthews">
 	<!-- Replace Name Surname with your name and surname -->
 </head>
+
 <body>
 	<div class="container">
-		<?php 
-			$query = "SELECT * FROM tbusers WHERE email = '$email' AND password = '$pass'";
-			$res = $mysqli->query($query);
-			if($row = mysqli_fetch_array($res))
-			{
-				echo 	"<table class='table table-bordered mt-3'>
+		<?php
+		$query = "SELECT * FROM tbusers WHERE email = '$email' AND password = '$pass'";
+		$res = $mysqli->query($query);
+		if ($row = mysqli_fetch_array($res)) {
+			echo 	"<table class='table table-bordered mt-3'>
 							<tr>
 								<td>Name</td>
 								<td>" . $row['name'] . "</td>
@@ -47,9 +88,9 @@
 								<td>" . $row['birthday'] . "</td>
 							<tr>
 						</table>";
-						
 
-				echo 	"<form action='' method='POST' enctype='multipart/form-data'>
+
+			echo 	"<form action='' method='POST' enctype='multipart/form-data'>
 							<div class='form-group'>
 								
 								<label for='eventName'>Event Name:</label><br>
@@ -60,20 +101,43 @@
 								<label for 'eventDate'>Event date:</label><br>
 								<input type='date' class='form-control' name='eventDate' /><br>	
 
-								<input type='file' class='form-control' name='picToUpload[]' id='picToUpload' multiple='multiple' /><br/>								
+								<input type='file' class='form-control' name='picToUpload[]' id='picToUpload' multiple='multiple' /><br/>	
+								
+								<input type='hidden' name='email' value='$email' />
+								<input type='hidden' name='pass' value='$pass' />
 
 								<input type='submit' class='btn-standard' value='Upload event' name='submit' />
 							</div>
-					  	</form>";				
-			}//end if
+					  	</form>";
 
-			else
-			{
-				echo 	'<div class="alert alert-danger mt-3" role="alert">
+			echo "<div class='container'>
+						<h2>UPCOMING EVENTS</h2>
+						<div class='row eventsGallery'>";
+			$res = $mysqli->query("SELECT * FROM tbevents e, tbgallery g WHERE e.user_id = $id AND e.event_id = g.event_id ORDER BY e.date DESC");
+			while ($res && $row = mysqli_fetch_array($res)) {
+				echo "<div class='card col-3 p-0'>
+							<div class='card-header'>
+								<p>" . $row['date'] . "</p>
+							</div>
+							<img src='gallery/" . $row['image_name'] . "' alt='thumbnail' />
+							<div class='card-body'>
+								
+								<h5>" . $row['name'] . "</h5>
+								<p class='card-text'>" . $row['description'] . "</p>
+							</div>
+						</div>";
+			}
+			echo "</div>
+					</div>";
+		} //end if
+
+		else {
+			echo 	'<div class="alert alert-danger mt-3" role="alert">
   							You are not registered on this site!
   						</div>';
-			}//end else
+		} //end else
 		?>
 	</div>
 </body>
+
 </html>
